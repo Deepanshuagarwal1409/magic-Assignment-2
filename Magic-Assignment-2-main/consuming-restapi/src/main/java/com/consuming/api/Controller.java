52b4a408a96d90b72e4bc8a2b86c00ca87f1acca
Deepanshu Agarwal <78287659+Deepanshuagarwal1409@users.noreply.github.com>
2022-03-15 09:48:24 +0530

    Update Controller.java

--- Magic-Assignment-2-main/consuming-restapi/src/main/java/com/consuming/api/Controller.java
+++ Magic-Assignment-2-main/consuming-restapi/src/main/java/com/consuming/api/Controller.java
@@ -1,19 +1,19 @@
 package com.consuming.api;
 
-//import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestController
 public class Controller {
 	
-//	private final AtomicLong counter = new AtomicLong();
+	private final AtomicLong counter = new AtomicLong();
 
 	private final EmployeeServiceApi employeeServiceApi;
 	
@@ -31,15 +31,15 @@ public class Controller {
 	    return employeeServiceApi.callAPI();
 	  }
 
-//	@GetMapping("/details")
-//	private OuterStatus getEmployeedetail() {
+	@GetMapping("/details")
+	private OuterStatus getEmployeedetail() {
 		
-//		String uri="http://dummy.restapiexample.com/api/v1/employee/1";
+		String uri="http://dummy.restapiexample.com/api/v1/employee/1";
 		
-//		RestTemplate restTemplate = new RestTemplate();
-//		OuterStatus outerstatus = restTemplate.getForObject(
-//				"http://dummy.restapiexample.com/api/v1/employee/1", OuterStatus.class);
-//		return outerstatus;
+		RestTemplate restTemplate = new RestTemplate();
+		OuterStatus outerstatus = restTemplate.getForObject(
+				"http://dummy.restapiexample.com/api/v1/employee/1", OuterStatus.class);
+		return outerstatus;
 		
 	}
 
